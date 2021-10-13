68ecdc0e6916a754f0812e5edd83f35bd76bf648
Erkin303 <maxmudjonoverkin55@gmail.com>
2021-10-13 08:03:57 +0500

    layoutlar qoyildi

--- pages/yangiHujjat/index.js
+++ pages/yangiHujjat/index.js
@@ -13,6 +13,36 @@ const YangiHujjat = () => {
                     <link rel="icon" href="/favicon.ico" />
                 </Head>
                 <h1 className='text-center'>yangi hujjat</h1>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-6 col-sm-12">
+                            <div>
+                                <form>
+                                    <label htmlFor="ism" className='mt-3 mb-1'>Ism</label>
+                                    <input type="text" className='form-control' placeholder="Ismoil" id="ism" />
+
+                                    <label htmlFor="familiya" className='mt-3 mb-1'>Familiya</label>
+                                    <input type="text" className='form-control' placeholder="Bahodirov" id="familiya" />
+                                </form>
+                            </div>
+                        </div>
+
+                        <div className="col-lg-6 col-sm-12">
+                            <form>
+                                <label htmlFor="passportid" className='mt-3 mb-1'>Passport ID</label>
+                                <input type="text" className='form-control' id="passportid" />
+
+                                <label htmlFor="file" className='mt-3 mb-1'>Hujjat yuklash</label>
+                                <input type="file" className='form-control' id="file" />
+
+
+                            </form>
+                            <div className='d-flex justify-content-end'>
+                                <button className='btn btn-primary mt-3'>Saqlash</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </YangiHujjatW>
         </All>
     )
