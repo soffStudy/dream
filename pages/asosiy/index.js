b62ef8febb3479619c11401d787d419d445ea4ab
Erkin303 <maxmudjonoverkin55@gmail.com>
2021-10-14 12:43:42 +0500

    asosiy page togirlandi

--- pages/asosiy/index.js
+++ pages/asosiy/index.js
@@ -3,6 +3,9 @@ import Head from 'next/head'
 import All from '../../components/all'
 import AsosiyW from './asosiyW'
 import Link from 'next/link'
+import { HiArrowSmRight } from "react-icons/hi";
+import { BiCalendarPlus, BiCalendarMinus, BiCalendarCheck, BiCalendarEdit } from "react-icons/bi";
+
 
 const Asosiy = () => {
     return (
@@ -17,48 +20,57 @@ const Asosiy = () => {
                 <div className="container">
                     <div className="row">
                         <div className="col-lg-4 col-md-6 col-sm-12">
-                            <Link href="table">
-                                <a>
-                                    <div className='asosiyCardlar'>
-                                        <p>Jami talabalr soni</p>
+                            <div className='asosiyCardlar'>
+                                <div className='d-flex justify-content-between'>
+                                    <p className='fs-3'><BiCalendarEdit /></p>
+                                    <p className='mt-2'>270</p>
+                                </div>
+                                <p>Bajaraib bo'lingan talabalar soni</p>
+                                <div className="d-flex justify-content-end">
+                                    <Link href="table">
+                                        <button className='btn btn-primary fs-5'>
+                                            <HiArrowSmRight />
+                                        </button>
+                                    </Link>
+                                </div>
+                            </div>
 
-                                        <div className="content">
-                                            <p>270</p>
-                                            <p>lorem ipsum</p>
-                                        </div>
-                                    </div>
-                                </a>
-                            </Link>
                         </div>
 
                         <div className="col-lg-4 col-md-6 col-sm-12">
-                            <Link href="bajarilishJarayon">
-                                <a>
-                                    <div className='asosiyCardlar'>
-                                        <p>Bajarilish jarayonidagi talabalr soni</p>
+                            <div className='asosiyCardlar'>
+                                <div className='d-flex justify-content-between'>
+                                    <p className='fs-3'><BiCalendarCheck /></p>
+                                    <p className='mt-2'>250</p>
+                                </div>
+                                <p>Bajarilish jarayonidagi talabalr soni</p>
+                                <div className="d-flex justify-content-end">
+                                    <Link href="bajarilishJarayon">
+                                        <button className='btn btn-primary fs-5'>
+                                            <HiArrowSmRight />
+                                        </button>
+                                    </Link>
+                                </div>
+                            </div>
 
-                                        <div className="content">
-                                            <p>250</p>
-                                            <p>lorem ipsum</p>
-                                        </div>
-                                    </div>
-                                </a>
-                            </Link>
                         </div>
 
                         <div className="col-lg-4 col-md-6 col-sm-12">
-                            <Link href="yuklamaganTalabalar">
-                                <a>
-                                    <div className='asosiyCardlar'>
-                                        <p>Tugallangan talablar soni</p>
+                            <div className='asosiyCardlar'>
+                                <div className='d-flex justify-content-between'>
+                                    <p className='fs-3'><BiCalendarMinus /></p>
+                                    <p className='mt-2'>20</p>
+                                </div>
+                                <p>Bajaraib bo'lingan talabalar soni</p>
+                                <div className="d-flex justify-content-end">
+                                    <Link href="yuklamaganTalabalar">
+                                        <button className='btn btn-primary fs-5'>
+                                            <HiArrowSmRight />
+                                        </button>
+                                    </Link>
+                                </div>
+                            </div>
 
-                                        <div className="content">
-                                            <p>20</p>
-                                            <p>lorem ipsum</p>
-                                        </div>
-                                    </div>
-                                </a>
-                            </Link>
                         </div>
                     </div>
                 </div>
--- styleW/asosiyW.js
+++ styleW/asosiyW.js
@@ -7,6 +7,12 @@ const AsosiyW = styled.div`
     background-color: #058347;
     border-radius: 10px;
     box-shadow: 5px 6px 10px 0 black;
+    color: white;
+    &:hover{
+        transition: 0.5s;
+        background-color: black;
+        transform: scale(1.3) translateY(20px);
+    }
 }
 
 a{
