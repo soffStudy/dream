b486760def5d2a270f9e6110a670bcd837eeac34
Erkin303 <maxmudjonoverkin55@gmail.com>
2021-10-16 19:51:41 +0500

    errorlar togirlandi

---
+++ pages/hujjatXodim3/index.js
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from 'react'
+import Dashboard2 from '../../components/dashboard2'
+import DocsWrapper from '../docs/docsW'
+import { useRouter } from 'next/router'
+import Head from 'next/head'
+import Data from '../xodimlar/dataXodimlar'
+import { AiFillPrinter } from "react-icons/ai";
+
+
+const Docs = () => {
+
+    const router = useRouter();
+    let fullData = {};
+    const [state, setstate] = useState(1);
+
+    useEffect(() => {
+        setstate(localStorage && localStorage.getItem("ID"));
+
+    }, []);
+
+
+    Data.map(value => {
+        if (value.id == state) fullData = value;
+    });
+
+    const printPageFun = (divName) => {
+        var printContents = document.getElementById(divName).innerHTML
+        var originalContents = document.body.innerHTML
+        document.body.innerHTML = printContents
+        window.print()
+        document.body.innerHTML = originalContents
+    }
+
+    return (
+        <Dashboard2>
+            <Head>
+                <title>Dream</title>
+                <meta name="description" content="dream education haqida toliq malumot" />
+                <link rel="icon" href="/favicon.ico" />
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+                <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@600&display=swap" rel="stylesheet"></link>
+            </Head>
+            <DocsWrapper>
+                <div className="d-flex justify-content-end me-3 mb-3" > <button className='btn btn-primary mt-2' onClick={() => printPageFun('print')}> <span className="fs-4" ><AiFillPrinter /></span> Pechat</button></div>
+                <div className="container docs2" id='print'>
+                    <h1>hujjat N3</h1>
+                    <h1>{fullData.id}  : id raqami</h1>
+                    <h1>{fullData.name}  : Ismi</h1>
+                </div>
+            </DocsWrapper>
+        </Dashboard2>
+    )
+}
+
+export default Docs;
---
+++ pages/xodimlar/dataXodimlar.js
@@ -0,0 +1,65 @@
+
+const DataXodim = [
+
+    {
+        name: "Aliyev Sherzod",
+        tel: "+998946910781",
+        direction: "Andijon viloyati",
+        lavozimi: "Xisobchi 1",
+        boshlaganVaqti: "15.02.2020",
+        xodimpassport:"de2.png",
+        id: "1"
+    },
+
+    {
+        name: "Nuriddinov alisher",
+        tel: "+998946000081",
+        direction: "Toshkent shahri",
+        lavozimi: "Xisobchi 2",
+        boshlaganVaqti: "15.05.2020",
+        xodimpassport:"de2.png",
+        id: "2"
+    },
+
+    {
+        name: "Boburov Xusan",
+        tel: "+998946955581",
+        direction: "Buxoro viloyati",
+        lavozimi: "Xisobchi 3",
+        boshlaganVaqti: "20.02.2018",
+        xodimpassport:"de2.png",
+        id: "3"
+    },
+
+    {
+        name: "Azizov Begzod",
+        tel: "+998952323636",
+        direction: "Sirdaryo viloyati",
+        lavozimi: "Xisobchi 4",
+        boshlaganVaqti: "28.10.2020",
+        xodimpassport:"de2.png",
+        id: "4"
+    },
+
+    {
+        name: "Rustamov Xojakbar",
+        tel: "+998946888881",
+        direction: "Samarqand viloyati",
+        lavozimi: "Xisobchi 5",
+        boshlaganVaqti: "09.07.2020",
+        xodimpassport:"de2.png",
+        id: "5"
+    },
+
+    {
+        name: "Doniyorov Akbar",
+        tel: "+998946910781",
+        direction: "Toshkent shahri",
+        lavozimi: "Xisobchi 1",
+        boshlaganVaqti: "15.02.2020",
+        xodimpassport:"de2.png",
+        id: "6"
+    },
+]
+
+export default DataXodim
